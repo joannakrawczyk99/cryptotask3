cd0d3fce70fd8567d66734122ec60c24b2fd90eb
Joanna Krawczyk <joannakrawczyk99@gmail.com>
2021-03-18 20:32:31 +0100

    explanation

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if checkPasswords(password1, password2):
     c = conn.cursor()
 
     db1 = addDb.DbFunctions(conn, c)
-    # db1.createTable()
+    # db1.createTable() #i did it only once
     db1.insertData(username, key, salt)
 
     verifyPassword(password1, salt, key)
