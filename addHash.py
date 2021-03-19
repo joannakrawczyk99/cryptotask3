09610f260cb6779ca28574604adc7b4b8ed7606d
Joanna Krawczyk <joannakrawczyk99@gmail.com>
2021-03-19 17:00:03 +0100

    storing-pass

--- addHash.py
+++ addHash.py
@@ -1,9 +1,31 @@
 import hashlib
 import os
 
-class UserProfile():
-    """Class UserProfile is for dealing with inputs that user has entered."""
+class UserProfile:
+    """
+    Class UserProfile is for dealing with inputs that user has entered.
+    ...
+    Attributes
+    ----------
+    username : str
+        username that selects the user
+    password : str
+        password that selects the user
+    Methods
+    -------
+     hashPassword(password):
+        makes hashes using pbkdf2_hmac from hashlib
+    """
     def __init__(self, username, password):
+        """
+        Construct all the necessary attributes for the userprofile object.
+
+        :param username: username that selects the user
+        :param password: password that selects the user
+
+        salt: str
+            generating unique, random salt for the password
+        """
         self.username = username
         self.password = password
         self.salt = os.urandom(32)
@@ -12,7 +34,11 @@ class UserProfile():
         return self.salt
 
     def hashPassword(self, password):
-        """Hashing using pbkdf2_hmac."""
+        """
+        Returns hashed password and salt
+        :param password: password that selects the user
+        :return: hashed password
+        """
         key = hashlib.pbkdf2_hmac(
             'sha256',
             str.encode(password),
