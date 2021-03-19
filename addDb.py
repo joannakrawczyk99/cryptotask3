fc81792d4566d610ac4d7c207374ff94dff39e6f
Joanna Krawczyk <joannakrawczyk99@gmail.com>
2021-03-19 17:05:52 +0100

    storing-pass

--- addDb.py
+++ addDb.py
@@ -1,17 +1,33 @@
 
 class DbFunctions:
-    """Class DbFunctions is for database controlling. It allows to create new table and insert data. """
+    """
+        Class DbFunctions is for database controlling. It allows to create new table and insert data.
+        ...
+        Attributes
+        ----------
+        connection :
+            let connect with the database
+
+        cursor :
+            let operate on the database
+
+        Methods
+        -------
+         createTableIfNotExists():
+            creates new table if does not exist
+
+        insertData():
+            add new record to the table
+        """
     def __init__(self, database, cursor):
         self.connection = database
         self.c = cursor
 
     def createTableIfNotExists(self):
-        """For creating a table that I need."""
         self.connection.execute('''CREATE TABLE IF NOT EXISTS passwords (username text, password text, salt text)''')
         self.connection.commit()
 
     def insertData(self, username, key, salt):
-        """For inserting data into the table created in previous method (createTable)."""
         sql = ''' INSERT INTO passwords (username, password, salt) VALUES (?,?,?)'''
         self.c.execute(sql, (username, key, salt))
         self.connection.commit()
